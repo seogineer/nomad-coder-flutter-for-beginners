80611eb6af465b37b2574bb4ebf3733bc52eab6f
서도경 <dogyeong.seo@emotion.co.kr>
2023-05-26 09:53:29 +0900

    fix: 오류 수정

--- toonflix/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ toonflix/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/seogineer/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/seogineer/Documents/workspace/nomad-coder-flutter-for-beginners/toonflix"
+export "FLUTTER_ROOT=C:\tools\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\workspace\nomad-coder-flutter-for-beginners\toonflix"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
